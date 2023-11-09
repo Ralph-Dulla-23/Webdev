70d1801a7ebcb25d4171688e97149c7d5ddabbdc
Ralph-Dulla-23 <rdulla_22000000260@uic.edu.ph>
2023-11-09 22:29:04 +0800

    Made landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,14 +10,73 @@ import { Column } from 'primereact/column';
 function App() {
   const [count, setCount] = useState(0)
 
+  
+
   return (
     <>
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/cs
+s2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48
+,100..700,0..1,-50..200" />
+
     <div className="WholeContent">
-      <div className="aside">
-          <aside>
-            <span>Hello</span>
+
+          <aside>  
+           <div className="aside">
+              
+ 
+              <div className="sidebar">
+                <div className="pfp">
+                
+                <h1>User!</h1>
+              </div>
+                <a href="#">
+                <span class="material-symbols-outlined">
+                  home
+                  </span>
+                  <h2>Dashboard</h2>
+                </a>
+                <a href="#">
+                <span class="material-symbols-outlined">
+                  book
+                  </span>
+                  <h2>Borrow</h2>
+                </a>
+                <a href="#">
+                <span class="material-symbols-outlined">
+                 keyboard_return
+                </span>
+                  <h2>Return</h2>
+                  </a>
+                   <a href="#">
+                   <span class="material-symbols-outlined">
+                    logout
+                    </span>
+                  <h2>Logout</h2>
+                </a>
+                
+              </div>
+            </div>
           </aside>
-        </div>
+        
+
+       
+          <div className="content">
+
+            <div className="upper">
+              
+               <div className="logo">
+                </div>
+                <h1>Welcome to GearGuard!</h1> 
+              </div>
+            
+            <div className="lower">
+
+            </div>
+            
+          </div>
+      
+
     </div>
     </>
   )
