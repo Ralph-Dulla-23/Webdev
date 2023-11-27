1dde68b95ff8bfb7baeebac4c52f2b1df46d5e71
Ralph-Dulla-23 <rdulla_22000000260@uic.edu.ph>
2023-11-27 22:36:26 +0800

    yes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,6 @@ import { Items } from './JavaScript/Items';
 
 function App() {
 
-  const [Items, setItems] = useState([]);
-
-  useEffect(() => {
-      Items.getItems().then(data => setItems(data));
-  }, []);
 
 
   const navigate = useNavigate();
@@ -82,12 +77,7 @@ s2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48
             
             <div className="lower">
                  <div className="table">
-                 <DataTable value={Items} tableStyle={{ minWidth: '50rem' }}>
-                <Column field="code" header="Code"></Column>
-                <Column field="name" header="Name"></Column>
-                <Column field="category" header="Category"></Column>
-                <Column field="quantity" header="Quantity"></Column>
-            </DataTable>
+                
                  </div>
             </div>
             
