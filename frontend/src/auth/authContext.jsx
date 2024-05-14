3e5d0f00a585dc3b13688aeb82a194cccfdf04d6
Ralph-Dulla-23 <rdulla_22000000260@uic.edu.ph>
2024-05-15 00:41:09 +0800

    sd

--- frontend/src/auth/authContext.jsx
+++ frontend/src/auth/authContext.jsx
@@ -6,19 +6,11 @@ export const AuthContext = createContext({
 
 function AuthProvider({ children }) {
   const [ID, setID] = useState("");
-<<<<<<< HEAD
   const [SelectedAvailableItem, setSelectedAvailableItem] = useState("");
   const [QuantitySelected, setSelectedQuantity] = useState("");
 
   return (
     <AuthContext.Provider value={{ ID, setID, SelectedAvailableItem, setSelectedAvailableItem, QuantitySelected, setSelectedQuantity}}>
-=======
-  const [SelectedItem, setSelectedItem] = useState("");
-  const [QuantitySelected, setSelectedQuantity] = useState("");
-
-  return (
-    <AuthContext.Provider value={{ ID, setID, SelectedItem, setSelectedItem, QuantitySelected, setSelectedQuantity}}>
->>>>>>> 17902798dd020ad6c45c7b8590e6511ac5eed12f
       {children}
     </AuthContext.Provider>
   )
